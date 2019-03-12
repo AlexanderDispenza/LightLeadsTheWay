81c4a118d0db0071964ffc11f3db69096cd1e136
AlexanderDispenza <alexanderdispenza@gmail.com>
2019-03-11 23:29:51 -0400

    Level Changer
    Added functioning  door mechanic. in order to proceed the player must meet the conditions. In this case a key is required to go to area 2

---
+++ Assets/Scripts/LevelChanger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LevelChanger : MonoBehaviour {
+
+    public KeyClick key;
+
+	// Use this for initialization
+	void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && key.hasKey == true)
+        {
+            SceneManager.LoadScene("area 2");
+        }
+    }
+
+}
